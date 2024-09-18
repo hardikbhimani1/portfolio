36ddecdf22b46d19c64d67b64060dc1c6dc26351
Hardik Bhimani <96721650+hardikbhimani1@users.noreply.github.com>
2024-09-18 23:38:18 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -75,16 +75,6 @@ function Home2() {
                   <AiFillGithub />
                 </a>
               </li>
-              {/* <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/hardik-bhimani-044492231?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <FaLinkedinIn />
-                </a>
-              </li> */}
               <li className="social-icons">
                 <a
                   href="https://www.instagram.com/hardik_bhimani_?igsh=MTR6Nm0zMjc5dzd1cQ=="
